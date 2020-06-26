cc23de8f333beee052a1d8e81c027d204ab0195d
Mohammad Haddad <66420319+HaddadGit@users.noreply.github.com>
2020-06-26 10:56:37 +0430

    create a new function called currentItem and fix the unique key warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Emojipedia from "../emojipedia";
 import Entery from "./Entery";
 
+function currentItem(props) {
+  return (
+    <Entery
+      key={props.id}
+      emoji={props.emoji}
+      name={props.name}
+      meaning={props.meaning}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{Emojipedia.map(Entery)}</dl>
+      <dl className="dictionary">{Emojipedia.map(currentItem)}</dl>
     </div>
   );
 }
